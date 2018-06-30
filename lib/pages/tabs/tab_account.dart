00b3c1f65722c86027e4839c302e847b87921f3b
Satit Riapit <siteslave@msn.com>
2018-06-30 16:20:55 +0700

    add listview

---
+++ lib/pages/tabs/tab_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabAccount extends StatefulWidget {
+  @override
+  _TabAccountState createState() => _TabAccountState();
+}
+
+class _TabAccountState extends State<TabAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Tab Account'),
+    );
+  }
+}
---
+++ lib/pages/tabs/tab_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabSetting extends StatefulWidget {
+  @override
+  _TabSettingState createState() => _TabSettingState();
+}
+
+class _TabSettingState extends State<TabSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Tab Setting'),
+    );
+  }
+}
