1cbb659d6bb6748ade91028e8e414a5518d2f79a
Satit Riapit <siteslave@msn.com>
2018-06-30 12:07:06 +0700

    add floating/bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,132 @@ class BasicWidgets extends StatefulWidget {
 }
 
 class _BasicWidgetsState extends State<BasicWidgets> {
+  // Step 2
+  int pageIndex = 0; // first page
+  List<Widget> pages = [HomePage(), AccountPage(), SettingPage()];
+
   @override
   Widget build(BuildContext context) {
     TextStyle myStyle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+
+    // Step 0
+    AppBar appBar = AppBar(
+      title: Text('Basic Scaffold'),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print('Account Clicked!');
+            }),
+        IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              print('Setting Clicekd!');
+            }),
+      ],
+    );
+
+    // Step 1
+    FloatingActionButton floatingActionButton = FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {},
+      backgroundColor: Colors.red,
+    );
+
+    // Step 2
+
+    BottomNavigationBar bottomNavigationBar = BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: (int index) {
+          setState(() {
+            pageIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text('Account')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), title: Text('Settings')),
+        ]);
+
+    // Step 3
+
+    BottomAppBar bottomAppBar = BottomAppBar(
+        hasNotch: false,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.purple,
+                ),
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 0;
+                  });
+                }),
+            IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.purple,
+                ),
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 2;
+                  });
+                }),
+          ],
+        ));
 
     return MaterialApp(
       title: 'Basic widgets',
       theme: ThemeData(primaryColor: Colors.purple),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Basic widgets'),
+        appBar: appBar,
+        body: pages[pageIndex],
+//        floatingActionButton: floatingActionButton,
+        floatingActionButton: FloatingActionButton.extended(
+          icon: const Icon(Icons.add),
+          label: const Text('Add'),
+          backgroundColor: Colors.purple,
+          onPressed: () {},
         ),
-        body: Center(
-          child: Text(
-            'Welcome to Flutter!!!',
-            style: myStyle,
-          ),
-        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//        bottomNavigationBar: bottomNavigationBar,
+        bottomNavigationBar: bottomAppBar,
       ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home page'),
+    );
+  }
+}
+
+class AccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Account page'),
+    );
+  }
+}
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Setting page'),
     );
   }
 }
